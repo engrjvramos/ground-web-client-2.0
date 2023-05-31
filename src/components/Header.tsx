10b21f0d6c5779d02410f0686d9bd3b439214cfc
JOBIE RAMOS <engrjvramos@gmail.com>
2023-05-31 09:53:48 +0800

    initialize files and folders

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+const navItems = {};
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <nav>
+          <ul></ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
